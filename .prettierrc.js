0aafd122f48a44068b99b05754c3a78213eda15f
tomaszbilka <bila4@op.pl>
2023-04-04 08:58:13 +0200

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  printWidth: 90,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
+
+// {
+//   "bracketSpacing": true,
+//   "embeddedLanguageFormatting": "auto",
+//   "htmlWhitespaceSensitivity": "css",
+//   "insertPragma": false,
+//   "jsxBracketSameLine": false,
+//   "jsxSingleQuote": false,
+//   "proseWrap": "preserve",
+//   "quoteProps": "as-needed",
+//   "requirePragma": false,
+//   "semi": true,
+//   "singleQuote": false,
+//   "tabWidth": 2,
+//   "trailingComma": "es5",
+//   "useTabs": false,
+//   "vueIndentScriptAndStyle": false,
+//   "printWidth": 100
+// }
