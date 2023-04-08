0fded8a3b406c9bacb44129dbbc579b5d9c27a5e
Tomasz <74007928+tomaszbilka@users.noreply.github.com>
2023-04-08 13:15:10 +0200

    add custom hook to fetch random img (#4)

---
+++ src/hooks/useFetchData.tsx
@@ -0,0 +1,33 @@
+import { useState, useCallback } from 'react';
+
+type TuseFetchData = [
+  sendRequest: (queryUrl: string, query?: string) => void,
+  data: string | null,
+  isLoading: boolean,
+  error: string | null,
+];
+
+const useFetchData = (): TuseFetchData => {
+  const [data, setData] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const sendRequest = useCallback(async (queryUrl, query) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${queryUrl}${query}`);
+      if (!response.ok) {
+        throw new Error('Catn fetch image!');
+      }
+      setData(response.url);
+      setIsLoading(false);
+    } catch {
+      setError('Sth went wrong!');
+      setIsLoading(false);
+    }
+  }, []);
+
+  return [sendRequest, data, isLoading, error];
+};
+
+export default useFetchData;
