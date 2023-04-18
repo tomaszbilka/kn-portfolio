554cc047bf3328f30d143b23173b28e1257f8af0
tomaszbilka <bila4@op.pl>
2023-04-18 21:23:05 +0200

    add source map to production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'bbdevpl',
+      project: 'react-advanced-devpath',
+      sourcemaps: {
+        assets: './dist/**',
+      },
+    }),
+  ],
 });
