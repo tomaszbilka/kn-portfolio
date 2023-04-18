069aa413a9b0d38e92d380c3b608f4dfba308dbd
tomaszbilka <bila4@op.pl>
2023-04-18 12:43:53 +0200

    fix ErrorPage

--- src/components/Errors/ErrorPage.tsx
+++ src/components/Errors/ErrorPage.tsx
@@ -7,9 +7,11 @@ const ErrorPage: React.FC = () => {
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error && 'Sth went wrong'}</i>
-      </p>
+      {error && (
+        <p>
+          <i>Sth went wrong</i>
+        </p>
+      )}
     </div>
   );
 };
