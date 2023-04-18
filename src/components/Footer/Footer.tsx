7dc82eb297997584d776eefaca6d51eef1e8387f
tomaszbilka <bila4@op.pl>
2023-04-18 20:46:44 +0200

    add sentry to app

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import styles from './Footer.module.css';
 
 const Footer: React.FC = () => {
-  return <footer className={styles.footer}>Footer text and copyright &copy;</footer>;
+  return <footer className={styles.footer}>Footer text and copyright &copy; </footer>;
 };
 
 export default Footer;
