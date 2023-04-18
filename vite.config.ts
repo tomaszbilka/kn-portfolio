a8e01dc682ee857a3990ebc8df2337e13d96f131
tomaszbilka <bila4@op.pl>
2023-04-18 21:55:20 +0200

    add .env and token to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    react(),
-    sentryVitePlugin({
-      org: 'bbdevpl',
-      project: 'react-advanced-devpath',
-      sourcemaps: {
-        assets: './dist/**',
-      },
-    }),
-  ],
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    build: {
+      sourcemap: true,
+    },
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        org: 'bbdev',
+        project: 'javascript-react',
+        authToken: env.SENTRY_AUTH_TOKEN,
+        sourcemaps: {
+          assets: './dist/**',
+        },
+      }),
+    ],
+  };
 });
