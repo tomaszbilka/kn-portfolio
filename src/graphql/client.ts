1e8a73a4717e3c299bf455546f60c4adc82eb035
tomaszbilka <bila4@op.pl>
2023-05-09 07:12:17 +0200

    imprt fragments in query insted of in client

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,17 +1,6 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
-import { createFragmentRegistry } from '@apollo/client/cache';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri: 'http://localhost:3008/',
-  cache: new InMemoryCache({
-    fragments: createFragmentRegistry(
-      gql`
-        fragment CoreUserFields on User {
-          name
-          lastName
-          address
-        }
-      `,
-    ),
-  }),
+  cache: new InMemoryCache(),
 });
